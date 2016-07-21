4ae945dd89e96467d21975a20e179ab19e7e6907
GGurtner <gurtner.gerald@gmail.com>
2016-07-21 14:29:26 +0100

    Improved evolution procedure

--- abm_strategic_model1/evolution.py
+++ abm_strategic_model1/evolution.py
@@ -16,7 +16,7 @@ from simulationO import do_standard, build_path as build_path_single
 from utilities import read_paras, post_process_paras
 from prepare_network import soft_infrastructure
 from abm_strategic_model1.iter_simO import average, loop
-from libs.general_tools import counter, clock_time
+from libs.general_tools import counter, clock_time, fit, r_squared
 from libs.paths import result_dir
 
 #pd.options.display.mpl_style = 'default'
@@ -52,7 +52,7 @@ class EvolutionCourse(object):
 		self.pop2 = def_pop2
 
 		if n_eq==None:
-			self.n_eq = int(n_iter/2.)
+			self.n_eq = min(int(n_iter/2.), 40)
 		else:
 			self.n_eq = n_eq
 
@@ -94,11 +94,37 @@ class EvolutionCourse(object):
 
 		return paras
 
+	def fit_nA(self):
+		initial_n = self.record['nA'][0]
+		def f_fit(x, a, b):
+			return initial_n + a*(1.-np.exp(-x/b))
+
+		popt, pcov, f_fit_opt = fit(list(range(len(self.record['nA']))), self.record['nA'],
+									f_fit=f_fit,
+									#p0=(0.3, 40.)
+									)
+
+		r2 = r_squared(self.record['nA'], np.vectorize(f_fit_opt)(self.record['nA']))
+
+		return popt, pcov, f_fit_opt, r2
+
 	def compute_results(self):
 		eq = np.mean(self.record['nA'][-self.n_eq:])
 		std = np.std(self.record['nA'][-self.n_eq:])
 
-		self.results = {'eq':eq, 'std':std}
+		eq_fit = np.mean(self.record['fitness'][-self.n_eq:])
+		std_fit = np.std(self.record['fitness'][-self.n_eq:])
+
+		# Compute time to equilibrium. This assumes an exponential
+		# relaxation and might not be super accurate.
+		# popt, pcov, f_fit_opt, r2 = self.fit_nA()
+		
+		# if r2>0.5:
+		# 	time = popt[1]
+		# else:
+		# 	time = np.nan
+
+		self.results = {'fS_eq':eq, 'fS_std':std, 'fit_eq':eq_fit, 'fit_std':std_fit, 'values':np.array(self.record['nA'])}
 
 	def compute_bounds_on_mix(self, eps=0.0001):
 		ntot = self.paras['ACtot']
@@ -136,7 +162,7 @@ def build_pat(paras, vers=main_version, in_title=['tau', 'par', 'ACtot', 'nA'], 
 	"""
 	Build the path for results.
 	"""
-	return build_path_single(paras, vers=vers, rep=rep) + '_iter' + str(paras['n_iter']) + '_evolution.pic'
+	return build_path_single(paras, vers=vers, rep=rep, name_G=paras['G'].name_generic) + '_iter' + str(paras['n_iter']) + '_evolution.pic'
 
 def do_evo((paras, kwargs)):
 	EC = EvolutionCourse(paras, **kwargs)
@@ -147,7 +173,8 @@ def do_evo((paras, kwargs)):
 def aggregate_results(results_list):
 	results = {}
 	for met in results_list[0].keys():
-		results[met] = {'avg':np.mean([v[met] for v in results_list]), 'std':np.std([v[met] for v in results_list])}
+		#print met, np.mean([v[met] for v in results_list], axis=0)
+		results[met] = {'avg':np.mean([v[met] for v in results_list], axis=0), 'std':np.std([v[met] for v in results_list], axis=0)}
 	
 	return results	
 
@@ -156,12 +183,40 @@ def iter_evolution(paras, **kwargs):
 	paras['n_iter_tot'] = n_iter_tot
 	print "Total number of iterations over the parameters:", n_iter_tot
 
-	loop({p:paras[p + '_iter'] for p in paras['paras_to_loop']}, paras['paras_to_loop'], \
-		paras, tot_lvl=len(paras['paras_to_loop']), thing_to_do=average,\
-		args_do=(paras, kwargs), do=do_evo, build_pat=build_pat, args_pat=(paras, ),\
-		aggregator=aggregate_results, parallel=paras['parallel'], force=paras['force'], \
-		n_iter=paras['n_iter'])
+	loop({p:paras[p + '_iter'] for p in paras['paras_to_loop']},
+			paras['paras_to_loop'],
+			paras,
+			tot_lvl=len(paras['paras_to_loop']),
+			thing_to_do=average,
+			args_do=(paras, kwargs), 
+			do=do_evo,
+			build_pat=build_pat,
+			args_pat=(paras, ),
+			aggregator=aggregate_results,
+			parallel=paras['parallel'],
+			force=paras['force'],
+			n_iter=paras['n_iter'])
+	print
 
+def load_results(paras=None, build_pat=None, args_pat=None, kwargs_pat={}, verbose=False):
+	file_pat = build_pat(*args_pat, **kwargs_pat)
+	with open(file_pat, 'r') as f:
+		results = pickle.load(f)
+	return results
+
+def get_results(paras, vers=main_version, show_detailed_evo=False):
+	it, results = loop({p:paras[p + '_iter'] for p in paras['paras_to_loop']}, 
+									paras['paras_to_loop'], 
+									paras,
+									results={},
+									thing_to_do=load_results,
+									paras=paras,
+									args_pat=(paras, ),
+									build_pat=build_pat,
+									kwargs_pat = {'vers':vers},
+									show_detailed_evo=show_detailed_evo)
+	print
+	return results
 
 if __name__=='__main__':
 	# paras_file = '../tests/paras_test_evolution.py'
@@ -185,12 +240,11 @@ if __name__=='__main__':
 
 	soft_infrastructure(paras['G'], paras_G)
 
-	paras['nA']=0.5
+	paras['nA'] = 0.5
 
 	print
 	paras['G'].describe(level=2)
 	print
-
 
 	if 0:
 		# save network instance
@@ -202,15 +256,27 @@ if __name__=='__main__':
 
 	print 'ACs:', paras['AC']
 
-	EC = EvolutionCourse(paras, n_iter=200, evolution_type='replicator')
+	save_rep = jn(result_dir, 'evolution/3.1')
+	os.system("mkdir -p " + save_rep)
+
+	EC = EvolutionCourse(paras, n_iter=100, evolution_type='replicator')
 	EC.run()
 
 	#print EC.record
 
+	# 
+	popt, pcov, f_fit_opt = EC.fit_nA()
+
+	print popt
+	print pcov
+	print np.trace(pcov)
+	print f_fit_opt(0)
+
 	# simple plot
 	plt.plot(range(EC.n_iter), EC.record['nA'], '-b')
-
-	eq = np.mean(EC.record['nA'][-100:])
+	plt.plot(range(EC.n_iter), np.vectorize(f_fit_opt)(list(range(EC.n_iter))), '-g')
+	eq = np.mean(EC.record['nA'][-40:])
 	plt.plot(range(EC.n_iter), [eq]*EC.n_iter, '--r')
 	#plt.plot(range(EC.n_iter), EC.record['fitness'], '-b')
+	plt.savefig(jn(save_rep, 'single_evolution_Dt1_d20.png'))
 	plt.show()
--- scripts/iter_evolution.py
+++ scripts/iter_evolution.py
@@ -1,17 +1,66 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 import sys
 sys.path.insert(1,'..')
 sys.path.insert(1,'../abm_strategic_model1')
+import os
 from os.path import join as jn
+import pickle
 
-from abm_strategic_model1.evolution import iter_evolution
-from libs.paths import main_dir
+from abm_strategic_model1.evolution import iter_evolution, get_results
+from abm_strategic_model1.iter_simO import iter_airport_change
+from libs.paths import main_dir, result_dir
 from abm_strategic_model1.utilities import read_paras
+from libs.general_tools import send_email_when_ready
 
 if __name__=='__main__':
-	paras_file = jn(main_dir, 'abm_strategic_model1/my_paras/my_paras_iter_for_DiskWorld_evolution.py')
-	paras = read_paras(paras_file=paras_file, post_process=True)
+	name_network = 'DiskWorld'
+	rep_results = 'evolution'
 
-	iter_evolution(paras, n_iter=20, evolution_type='replicator')
+	send_email = False
+
+	paras_G_file = jn(main_dir, 'abm_strategic_model1/my_paras/my_paras_G_iter_DiskWorld_evolution.py')
+	paras_file = jn(main_dir, 'abm_strategic_model1/my_paras/my_paras_iter_for_DiskWorld_evolution.py')
+
+	with send_email_when_ready(do=send_email, text='Finished simulations with ' + name_network + ' (' + 'evolution' + ')\n\n'):
+		if 1:
+			file_net = jn(result_dir, 'networks/' + name_network + '/' + name_network + '.pic')
+			with open(file_net, 'r') as f:
+				G = pickle.load(f)
+
+			paras_G_iter = read_paras(paras_file=paras_G_file, post_process=False)
+			iter_airport_change(paras_G_iter, G)
+
+		if 1:
+			paras = read_paras(paras_file=paras_file, post_process=True)
+			iter_evolution(paras, n_iter=200, evolution_type='replicator')
+
+		# Save paras files 
+		rep = jn(result_dir, 'model1/3.1/' + name_network + '/' + rep_results)
+		os.system('mkdir -p '+ rep)
+		# Save iter paras of network builder
+		# os.system('cp ' + paras_G_file + ' ' + rep + '/')
+		# print "Copied", paras_G_file, "in", rep
+		# # Save paras of network builder
+		# os.system('cp ' + paras_G_iter['paras_file'] + ' ' + rep + '/')
+		# print "Copied", paras_G_iter['paras_file'], "in", rep
+		# Save iter paras of simulation
+		os.system('cp ' + paras_file + ' ' + rep + '/')
+		print "Copied", paras_file, "in", rep
+		# Save paras of simulation
+		os.system('cp ' + paras['paras_file'] + ' ' + rep + '/')
+		print "Copied", paras['paras_file'], "in", rep
+
+		# Gather results and dump them on disk for external plotting
+		print "Preparing results..."
+		results = get_results(paras, show_detailed_evo=True)
+
+		#print results
+
+		with open(jn(rep, 'results.pic'), 'w') as f:
+			pickle.dump(results, f)
+
+		print "Files saved in:", rep
+
 
